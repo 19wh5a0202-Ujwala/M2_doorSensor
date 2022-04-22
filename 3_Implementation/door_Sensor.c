e23bd899cba80fb00130539fd4c68f56e1b82069
19wh5a0202-Ujwala <88813499+19wh5a0202-Ujwala@users.noreply.github.com>
2022-04-22 09:59:54 +0530

    Add files via upload

---
+++ 3_Implementation/door_Sensor.c
@@ -0,0 +1,22 @@
+/*
+ * door_sensor.c
+ *
+ * Created: 22-04-2022 7.43.55 AM
+ * Author : K.UJAWALA
+ */ 
+
+#include <avr/io.h>
+int main(void)
+{
+	DDRB=DDRB&0b11111101;//fd
+	DDRC=DDRC|0b01000000;//40
+	while(1)
+	{
+		if(PINB & 0b00000010)//02
+		PORTC=PORTC|0b01000000;//40
+		else
+		PORTC=PORTC&0b10111111;//bf
+	}
+	return 0;
+}
+
